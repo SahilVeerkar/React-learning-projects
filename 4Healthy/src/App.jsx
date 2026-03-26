cf2c5abac9aeb192df96fba90b63989d2c30535c
user.name:"sahil veerkar <user.email:"veerkarsahil@gmail.com>
2026-03-27 01:02:15 +0530

    sencond phase complete

--- 4Healthy/src/App.jsx
+++ 4Healthy/src/App.jsx
@@ -1,20 +1,21 @@
-
+import FoodItems from './components/FoodItems';
+import ErrorMessage from './components/ErrorMessage';
 import 'bootstrap/dist/css/bootstrap.min.css';
+ import "./App.css";
 function App() {
+
  let foodItems=["Dal","Green Vegetable","Roti","Salad","Milk","Ghee" ]
 
-  return (
-    <>
-   <h1>Healthy Food</h1>
-     <ul className="list-group">
-      {foodItems.map((item)=>(
- <li className="list-group-item" key={item}>{item}</li>
-      ))}
- 
- 
-</ul>
-</>
-  )
+  return (<>
+   <h1 className='food-heading'>Healthy Food</h1>
+   
+   <ErrorMessage items={foodItems}></ErrorMessage>
+   
+   <FoodItems items={foodItems} ></FoodItems>
+   
+   
+     
+</>); 
 }
 
-export default App
+export default App;
