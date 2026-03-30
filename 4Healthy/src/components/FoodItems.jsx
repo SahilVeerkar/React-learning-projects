06999195ff5932214fe582ed5d6816ce31a51211
sahilVeerkar <veerkarsahil@gmail.com>
2026-03-30 13:04:33 +0530

    5th phase complete

--- 4Healthy/src/components/FoodItems.jsx
+++ 4Healthy/src/components/FoodItems.jsx
@@ -1,11 +1,25 @@
 import Item from "./Item";
+import { useState } from "react";
 
 const FoodItems=({items})=>{
-   
+ 
+  let [activeItem,setActiveItem]=useState([]); 
+  
+  let onBuyButton=(item, event)=>{
+let newItems=[...activeItem,item]
+setActiveItem(newItems);
+  }
+
 return(<>
 <ul className="list-group">
       {items.map((item)=>(
-        <Item key={item} foodItems={item} handleBuyButtonClicked={()=>( console.log(`${item} buy `) )}></Item>
+        <Item
+        bougth={activeItem.includes(item )}
+        key={item} 
+        foodItems={item} 
+        handleBuyButtonClicked={(event)=>onBuyButton(item,event)}
+        
+        ></Item>
       ))}
 
  </ul>
--- 4Healthy/src/components/Item.jsx
+++ 4Healthy/src/components/Item.jsx
@@ -1,10 +1,11 @@
 import styles from "./Item.module.css";
-const Item=({foodItems , handleBuyButtonClicked})=>{
+const Item=({foodItems ,bougth, handleBuyButtonClicked})=>{
 //   const handleBuyButtonClicked=(foodItems)=>{
 // console.log(`${foodItems} being bought`)
 //   }
 return(<>
-<li className={`${styles['kg-item']} list-group-item`} ><span className={styles['kg-span']}>{foodItems}</span>
+<li className={`${styles['kg-item']} list-group-item  ${bougth && "active"}`} >
+  <span className={styles['kg-span']}>{foodItems}</span>
 <button className={`${styles.buttons} btn btn-info`}
 onClick={handleBuyButtonClicked}
 >Buy</button>
