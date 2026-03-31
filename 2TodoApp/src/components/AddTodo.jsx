46b5bd5f7cc18e46dcc7b40b49e651cbbbdb8d1b
sahilVeerkar <veerkarsahil@gmail.com>
2026-03-31 17:39:21 +0530

    use ref use

--- 2TodoApp/src/components/AddTodo.jsx
+++ 2TodoApp/src/components/AddTodo.jsx
@@ -1,32 +1,50 @@
 import styles from "./AddTodo.module.css"
-import { useState } from "react";
+import {useRef } from "react";
 import { IoIosAdd } from "react-icons/io";
 function AddTodo({onNewItem}){
-  const [todoName,SetTodoName]=useState("");
-  const [dueDate,setDueDate]=useState("");
+  // const [todoName,SetTodoName]=useState("");
+  // const [dueDate,setDueDate]=useState("");
+  const todoNameElement=useRef(0);
+  const dueDateElement=useRef(0);
+  
 
-  const handleNameChange=(event)=>{
-    SetTodoName(event.target.value);
-  }
-    const handleDateChange=(event)=>{
-    setDueDate(event.target.value);
-  }
+
+  // const handleNameChange=(event)=>{
+  //   SetTodoName(event.target.value);
+  //   noOfUpdate.current+=1;
+  // }
+  //   const handleDateChange=(event)=>{
+  //   setDueDate(event.target.value);
+  //   console.log(noOfUpdate.current)
+  // }
   const handleAddButton=()=>{
+    const todoName=todoNameElement.current.value;
+    const dueDate=dueDateElement.current.value;
+    dueDateElement.current.value="";
+    todoNameElement.current.value="";
 onNewItem(todoName ,dueDate)
-setDueDate("");
-SetTodoName("");
+// setDueDate("");
+// SetTodoName("");
   }
 return(<>
 <div className="container text-center"> 
   <div className="row kg-row">
     <div className="col-6">
-    <input className={styles.in} type="text" value={todoName} placeholder="enter todo" onChange={handleNameChange}/>
+    <input className={styles.in} ref={todoNameElement} type="text" 
+    //value={todoName} 
+    placeholder="enter todo" 
+    //onChange={handleNameChange}
+    />
     </div>
     <div className="col-4">
-      <input className={styles.in} type="date" value={dueDate} onChange={handleDateChange}/>
+      <input className={styles.in} ref={dueDateElement} type="date" 
+      //value={dueDate} 
+      //onChange={handleDateChange}
+      />
     </div>
     <div className="col-2">
-      <button type="button" className="btn btn-success kg-button" onClick={handleAddButton}><IoIosAdd></IoIosAdd></button>
+      <button type="button" className="btn btn-success kg-button" 
+      onClick={handleAddButton}><IoIosAdd></IoIosAdd></button>
     </div>
   </div>
   </div>
--- 2TodoApp/src/components/TodoItem.jsx
+++ 2TodoApp/src/components/TodoItem.jsx
@@ -4,12 +4,7 @@ function TodoItem({todoName,todoDate,onDeleteClick}){
   
 return(<>
 <div className="container"> 
-
-        
-   
-  
-
-   <div className="row kg-row">
+<div className="row kg-row">
     <div className="col-6">{todoName}</div>
     <div className="col-4">{todoDate}</div>
     <div className="col-2 side">
