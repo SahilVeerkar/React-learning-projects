b03ae60a4390b943af1dc3e90223c6a232d749be
sahilVeerkar <veerkarsahil@gmail.com>
2026-05-11 01:01:18 +0530

    social media first part

---
+++ 6socialMedia/src/App.jsx
@@ -0,0 +1,31 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Sidebar from './components/Sidebar';
+import CreatePost from './components/CreatePost';
+import Postlist from './components/Postlist';
+import { useState } from 'react';
+
+function App() {
+  const [selectedTab,setSelectedTab]=useState("Home");
+
+  return <>
+  <div className='app-container'>
+      <Sidebar selectedTab = {selectedTab}></Sidebar>
+      <div className='content'>
+        <Header></Header>
+        {selectedTab==='Home' ? <Postlist></Postlist> :  <CreatePost></CreatePost>}
+       
+        
+      <Footer></Footer>
+
+      </div>
+      </div>
+ 
+      
+    </>
+  
+}
+
+export default App
---
+++ 6socialMedia/src/components/Postlist.jsx
@@ -0,0 +1,9 @@
+import Post from "./Post";
+const Postlist=()=>{
+return<>
+<Post></Post>
+<Post></Post>
+<Post></Post>
+</>
+}
+export default Postlist;
