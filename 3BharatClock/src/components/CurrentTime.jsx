d5f4abff5f3e0d1af753f7a4341755de744341cd
sahilVeerkar <veerkarsahil@gmail.com>
2026-06-21 11:47:11 +0530

    use effect and setinterval,clearinterval used

--- 3BharatClock/src/components/CurrentTime.jsx
+++ 3BharatClock/src/components/CurrentTime.jsx
@@ -1,7 +1,18 @@
+import { useEffect, useState } from "react";
+
 let CurrentTime=()=>{
-  let time = new Date();
+  const[time,setTime]=useState(new Date());
+  useEffect(()=>{
+    const interValId = setInterval(()=>{
+      setTime(new Date());
+    },1000);
+    return()=>{
+ clearInterval(interValId);
+    }
+   
+  },[])
 return(<>
 <p className="lead">this is the current time : {time.toLocaleDateString()} - {time.toLocaleTimeString()}</p>
 </>)
 }
-export default CurrentTime
+export default CurrentTime;
