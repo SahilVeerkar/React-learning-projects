7a42c782c9a4761490e084f10e7a69b8635b1751
sahilVeerkar <veerkarsahil@gmail.com>
2026-06-21 08:27:44 +0530

    abortControl update

--- 6socialMedia/src/components/Postlist.jsx
+++ 6socialMedia/src/components/Postlist.jsx
@@ -11,11 +11,17 @@ const Postlist=()=>{
   
  useEffect(()=>{
   setFetching(true);
- fetch('https://dummyjson.com/posts')
+  const controller=new AbortController();
+  const signal=controller.signal;
+ fetch('https://dummyjson.com/posts',{signal})
 .then(res => res.json())
 .then((data)=>{
 addInitialPosts(data.posts);
 setFetching(false);
+
+return()=>{
+  controller.abort();
+}
 
 });
  },[]);
